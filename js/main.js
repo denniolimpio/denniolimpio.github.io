ba6f4ed0e6847b67ce588dcf15309d452c22de79
dennis <dennis.olimpio0794@gmail.com>
2020-11-13 01:04:49 +0100

    versione 1
    versione 1

---
+++ js/main.js
@@ -0,0 +1,166 @@
+// esercizio
+// creare uno slider di immagini e personalizzare l'html
+
+//apro document
+$(document).ready (
+
+
+  function() {
+
+    $('body').append('<div style="" id="loadingDiv"><div class="loader">Loading...</div></div>');
+    $(window).on('load', function(){
+      setTimeout(removeLoader, 2000); //wait for page load PLUS two seconds.
+    });
+    function removeLoader(){
+      $( "#loadingDiv" ).fadeOut(500, function() {
+        // fadeOut complete. Remove the loading div
+        $( "#loadingDiv" ).remove(); //makes page more lightweight
+      });
+    }
+
+
+    ////////////
+    ////////////
+    ////////////
+    ////////////
+    ////////////
+    ////////////
+    ////////////
+
+    $(".ms_bars").click(function() {
+      alert("click");
+    });
+
+
+
+    $(".with-dropdown").mouseenter(
+      function(){
+
+        $(this).children(".dropdown").addClass("active");
+
+
+      });
+
+      // Se sposto  il mouse da un elemento con dropdown visibile  questo si chiude
+
+      $(".with-dropdown").mouseleave(
+
+        function () {
+          $(this).children(".dropdown").removeClass("active");
+
+        });
+
+
+        // Con un doppio click chiudo il menu a comparsa se è aperto,
+        // altrimenti lo apro.
+
+        $(".with-dropdown").dblclick(
+
+          function(){
+
+            if( $(".dropdown").hasClass("active") ) {
+              $(this).children(".dropdown").removeClass("active");
+
+            } else {
+              $(this).children(".dropdown").addClass("active");
+
+            }
+          })
+          // cliccando sul tasto freccia destra genero un evento
+
+          // slider img successiva
+          $(".next").click (
+
+            function() {
+              immagineSuccessiva();
+
+            });
+
+            // cliccando sul tasto freccia sinistra genero un evento
+
+            // slider img precedente
+            $(".prev").click (
+
+              function() {
+                immaginePrecedente();
+
+
+              });
+
+
+
+            });
+
+            //funzioni
+
+            //ho bisogno di una funzione che, cliccando sulla freccia dx, mi permetta di passare dall'immagine corrente a quella successiva
+            // questa stessa funzione deve evidenziare il cerchietto dell'immagine che è visibile in quel momento.
+
+
+            // #funzione1
+            function immagineSuccessiva() {
+
+
+              // definisco la mia variabile riferita all'immagine che al momento risulta selezionata,visibile
+              var immagineSelezionata = $(" .images img.active ");
+              var cerchioAttivo = $( ".nav i.active");
+
+              // rimuovo la classe " active" all'elemento che al momento è visibile e(..)
+              immagineSelezionata.removeClass("active");
+              cerchioAttivo.removeClass("active");
+
+
+              //Se immagineSelezionata e cerchioAttivo ha classe "last", allora aggiungo al'elemento con classe "first" , la classe "active".
+              //In questo modo dopo l'ultima immagine riappare la prima.
+
+              if(immagineSelezionata.hasClass("last")){
+                $(".images img.first").addClass("active");
+                $(".nav i.first").addClass("active");
+
+              } else {
+                // aggiungo la classe " active" all'immagine successiva.
+
+                immagineSelezionata.next().addClass("active");
+                cerchioAttivo.next().addClass("active");
+
+              }
+
+            };
+
+
+            //ho bisogno di una funzione che, cliccando sulla freccia sx, mi permetta di passare dall'immagine corrente a quella precedente
+            // questa stessa funzione deve evidenziare il cerchietto dell'immagine che è visibile in quel momento.
+
+            // #funzione 2
+
+
+            function immaginePrecedente() {
+
+              // definisco la mia variabile riferita all'elemento che al momento risulta selezionata,visibile
+
+              var immagineSelezionata = $(".images img.active");
+              var cerchioAttivo = $( ".nav i.active");
+
+              // rimuovo la classe " active" all'elemento che al momento è visibile e(..)
+              immagineSelezionata.removeClass("active");
+              cerchioAttivo.removeClass("active");
+
+
+              //Se immagineSelezionata e cerchioAttivo  ha classe "first", allora aggiungo al'elemento con classe "last" , la classe "active"
+              //In questo modo dopo la prima immagine riappare l'ultima.
+
+
+              if(immagineSelezionata.hasClass("first")) {
+                $(".images img.last").addClass("active");
+
+                $ (".nav i.last").addClass("active");
+
+              } else {
+
+
+                immagineSelezionata.prev().addClass("active");
+                cerchioAttivo.prev().addClass("active");
+              }
+            }
+
+            //chiudo document

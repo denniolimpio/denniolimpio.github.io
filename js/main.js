779343526404929c6b21818b69435ccf57831542
dennis <dennis.olimpio0794@gmail.com>
2020-11-14 15:48:06 +0100

    burger v3

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ $(document).ready (
   function() {
 
     $('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
+      $(".burger").animate({width:'toggle'},350);
 		$(this).toggleClass('open');
     $('.burger').toggleClass('visibile');
 
